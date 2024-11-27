967dcf2e2ea84b16acdba2791d861a030ec37658
Gregor Weber <mail@watwa.re>
2024-11-27 23:49:26 +0100

    attempt sourcemaps

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,5 +1,12 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  webpack(config, { isServer }) {
+    if (isServer) {
+      config.devtool = "source-map";
+    }
+    return config;
+  },
+};
 
 export default nextConfig;
